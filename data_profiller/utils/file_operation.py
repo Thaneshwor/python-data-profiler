ee474cb14d233f42187a61cc93a04261e8231888
Thaneshwor <thaneshwor100@gmail.com>
2019-09-04 17:39:59 +0545

    Change logic for returning dataframe

--- data_profiller/utils/file_operation.py
+++ data_profiller/utils/file_operation.py
@@ -1,53 +1,24 @@
 import csv
 import pandas as pd
-import itertools
-import sys
+
+from utils.cleanser import replace_nan
 
 
 def get_dataframe(file_name):
     ''' Return dataframe of file after reading it. '''
+    fs = find_seperator(file_name)
 
-    file_type = find_file_type(file_name)
-
-    if file_type == 'csv':
-        return pd.read_csv(file_name, delimiter=',', low_memory=False)
-    elif file_type == 'fwf':
-        return pd.read_fwf(file_name, low_memory=False)
-    elif file_type == 'tsv':
-        return pd.read_csv(file_name, sep='\t', low_memory=False)
+    if len(fs) > 1:
+        df = pd.read_fwf(file_name)
     else:
-        print('File Error Please provide csv, fwf, tsv or fwf(txt)')
+        df = pd.read_csv(file_name, sep=fs, low_memory=False)
+
+    return df
 
 
-def get_file_extension(s, delim):
-    ''' Return file extension . '''
-    return s.partition(delim)[2]
-
-
-def find_file_type(file_name):
-    ''' Return type of file. '''
-    ext = get_file_extension(file_name, '.')
-
-    if ext == 'csv':
-        file_type = find_seperator(file_name)
-
-        if file_type == ',':
-            return 'csv'
-
-        elif file_type == '\t':
-            return 'tsv'
-        else:
-            print('Fixed Width File(CSV)')
-            return 'fwf'
-    elif ext == 'tsv' or ext == 'txt':
-        print('TSV File')
-        return 'tsv'
-
-    return 'file_error'
-
-
-def find_seperator(filename):
-    with open(filename, 'r') as myCsvfile:
+def find_seperator(file_name):
+    ''' Return seperator of file.'''
+    with open(file_name, 'r') as myCsvfile:
         header = myCsvfile.readline()
         if header.find(";") != -1:
             return ";"
@@ -55,5 +26,8 @@ def find_seperator(filename):
             return ","
         if header.find("\t") != -1:
             return "\t"
+        if header.find("|") != -1:
+            return "|"
+
     # default delimiter (MS Office export)
-    return ";"
+    return "fwf"
