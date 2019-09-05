595a35989e5709690dfaaf475066ffc1e0b9dbe6
Thaneshwor <thaneshwor100@gmail.com>
2019-09-05 11:10:22 +0545

    Add code for displaying error.log file

--- data_profiller/utils/file_operation.py
+++ data_profiller/utils/file_operation.py
@@ -1,7 +1,11 @@
 import csv
 import pandas as pd
 
+from utils.logger import setup_logger
 from utils.cleanser import replace_nan
+
+
+error_logger = setup_logger('error_logger', 'error.log')
 
 
 def get_dataframe(file_name):
@@ -13,6 +17,7 @@ def get_dataframe(file_name):
     else:
         df = pd.read_csv(file_name, sep=fs, low_memory=False)
 
+    print(df)
     df = replace_nan(df)
 
     return df
@@ -20,16 +25,20 @@ def get_dataframe(file_name):
 
 def find_seperator(file_name):
     ''' Return seperator of file.'''
-    with open(file_name, 'r') as myCsvfile:
-        header = myCsvfile.readline()
-        if header.find(";") != -1:
-            return ";"
-        if header.find(",") != -1:
-            return ","
-        if header.find("\t") != -1:
-            return "\t"
-        if header.find("|") != -1:
-            return "|"
+    try:
+        with open(file_name, 'r') as myCsvfile:
+            header = myCsvfile.readline()
+            if header.find(";") != -1:
+                return ";"
+            if header.find(",") != -1:
+                return ","
+            if header.find("\t") != -1:
+                return "\t"
+            if header.find("|") != -1:
+                return "|"
+    except Exception as e:
+        print('error occuure---------------------', e)
+        error_logger.error(e)
 
     # default delimiter (MS Office export)
     return "fwf"
